df3b350dd2cd34bc5000693143c9be3270b77f6d
anis F <anisbegitu@gmail.com>
2021-12-08 13:28:03 +0700

    deploydb

--- models/setup.go
+++ models/setup.go
@@ -11,9 +11,9 @@ func ConnectDatabase() {
 	dsn := "root:p4ssw0Rd@tcp(localhost:3306)/db_api_jwt?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	if err != nil {
-		panic("Failed to connect to database!")
-	}
+	// if err != nil {
+	// 	panic("Failed to connect to database!")
+	// }
 
 	database.AutoMigrate(&Product{})
 	database.AutoMigrate(&Transaction{})
